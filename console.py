79d6cf4f42b586dfeaa09813cefc4a777855894a
Muigai-ben <muigaibenson37@gmail.com>
2023-11-11 03:21:49 -0800

    console.py

---
+++ console.py
@@ -0,0 +1,20 @@
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print("\nExiting the HBNB command console.")
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
